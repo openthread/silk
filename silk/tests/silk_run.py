0d79e87f8f207e5f5f35e3130327f9c7e5332125
Colin Tan <tancolin@google.com>
2020-07-06 16:43:38 -0400

    Add OTNS addr arg

--- silk/tests/silk_run.py
+++ silk/tests/silk_run.py
@@ -185,6 +185,9 @@ class SilkRunner(object):
             help='Set the verbosity level of the console (0=quiet, 1=default, 2=verbose)')
         parser.add_argument('pattern', nargs='+', metavar='P',
             help='test file search pattern')
+        parser.add_argument('-o', '--otns', dest='otns_server',
+            metavar='OtnsServer',
+            help='OTNS server address')
         return parser.parse_args(argv[1:])
 
     def discover(self):
